b01daeadcd03e9a08805ba65abfb9dd122fd986a
Mewlala <mewlapatrada@gmail.com>
2024-06-22 15:21:44 +0700

    Initial commit

---
+++ app/Blog/page.tsx
@@ -0,0 +1,16 @@
+export default function Blog() {
+  return(
+    <div className="justify-center">
+    <div className="m-6 h-[120px] w-[400px] rounded-lg bg-blue-50">
+      <div className="flex flex-wrap">
+        <div><img src="https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg" width="40" /></div>
+        <div className="flex-col">
+          <div>Lapatrada Chotirat</div>
+          <div className="text-xs">Jun 22</div>
+        </div>
+      </div>
+      <div className="text-center">How you like that!!!</div>
+      <div className="text-xs leading-10 text-pink-500">#blackpink #Lisa #Jennie #Jisoo #Rose</div>
+    </div>
+  </div>
+)}
